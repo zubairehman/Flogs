d428fc18ab19bca22fd2acd4ee999b75c7e54d46
Markus Markowski <markus.markowski@vwgis.de>
2019-08-18 15:28:54 +0200

    Added fieldOrderFormatCustom for order fields in customized output, added TIME_FORMAT_FULL_3

--- lib/model/flog/flog_config.dart
+++ lib/model/flog/flog_config.dart
@@ -1,5 +1,6 @@
 import 'package:f_logs/model/datalog/data_log_type.dart';
 import 'package:f_logs/model/flog/log_level.dart';
+import 'package:f_logs/utils/formatter/field_name.dart';
 import 'package:f_logs/utils/formatter/formate_type.dart';
 import 'package:f_logs/utils/timestamp/timestamp_format.dart';
 
@@ -18,15 +19,24 @@ class LogsConfig {
     LogLevel.FATAL,
     LogLevel.OFF
   ]; //Your set of enabled levels, like INFO, WARNING, ERROR, SEVERE, ...
-  // default is all levels (compatibility) to previous version
+  // default is all levels (compatibility to previous version)
   var activeLogLevel =
       LogLevel.INFO; //Active log level, lower levels are skipped
-  // default is LogLevel.INFO (compatibility) to previous version
+  // default is LogLevel.INFO (compatibility to previous version)
   var dataLogTypes = [
     DataLogType.DEFAULT.toString()
   ]; //Like Network, Database, Location etc
   var formatType =
       FormatType.FORMAT_CURLY; //Default format of '{String}' log data
+  var fieldOrderFormatCustom = [
+    FieldName.CLASSNAME,
+    FieldName.METHOD_NAME,
+    FieldName.TEXT,
+    FieldName.EXCEPTION,
+    FieldName.LOG_LEVEL,
+    FieldName.TIMESTAMP,
+    FieldName.STACKTRACE
+  ]; // Field order for output
   var customOpeningDivider = " "; //For log field open
   var customClosingDivider = " "; //For log field close
   var csvDelimiter = ""; //Delimiter for CSV files
